65d45050ff986b6046c5f7633496159e8c021aa0
leaceliku96 <43318791+leaceliku96@users.noreply.github.com>
2020-09-28 13:22:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 <html>
   
   <head>Canary Example</head>
-
-  <?php
-
-  for ($count = 0; $count <= 1000; $count++){
   
-    echo "Hello World 2021!";
+  <body>
 
-  }
+  
+    <?php
+    
+      for ($count = 0; $count <= 1000; $count++){
+  
+      echo "Hello World 2021!";
+
+      }
 
   ?>
+    
+  </body>
 
 </html>
